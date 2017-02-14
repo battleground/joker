d4c9497d7dbe27165d661f37585c642500100d6a
大宇 <allnet@live.cn>
2017-02-14 14:39:24 +0800

    增加：增加应用权限；增加ScannerSamples.java

---
+++ joker-dialog/src/main/java/com/abooc/joker/dialog/ScannerSamples.java
@@ -0,0 +1,66 @@
+package com.abooc.joker.dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Handler;
+
+import com.abooc.upnp.DlnaManager;
+import com.abooc.upnp.model.DeviceDisplay;
+
+/**
+ * Created by dayu on 2017/2/14.
+ */
+
+public class ScannerSamples extends UPnPPresenter {
+
+    public ScannerSamples(Viewer viewer, Handler handler) {
+        super(viewer, handler);
+    }
+
+    public static void show(Context context) {
+        final ScanningDialog iScanningDialog = new ScanningDialog(context);
+        final UPnPPresenter iUPnPPresenter = new UPnPPresenter(iScanningDialog, new Handler());
+        iScanningDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                iScanningDialog.setOnSelectedDeviceListener(registerEvent(iScanningDialog));
+
+                iUPnPPresenter.start();
+            }
+        });
+
+        iScanningDialog.show();
+
+        iScanningDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+
+                iUPnPPresenter.quit();
+            }
+        });
+
+    }
+
+    static ScanningDialog.OnSelectedDeviceListener registerEvent(final ScanningDialog iScanningDialog) {
+        return new ScanningDialog.OnSelectedDeviceListener() {
+            @Override
+            public void onSelectedDevice(DeviceDisplay device) {
+
+                boolean bind = DlnaManager.getInstance().bind(device.getOriginDevice(), null);
+                if (bind) {
+                    device.setChecked(true);
+
+                    iScanningDialog.notifyDataSetChanged();
+                }
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        iScanningDialog.dismiss();
+                    }
+                }, 300);
+            }
+        };
+    }
+
+}
--- joker-dialog/src/main/java/com/abooc/joker/dialog/samples/SamplesActivity.java
+++ joker-dialog/src/main/java/com/abooc/joker/dialog/samples/SamplesActivity.java
@@ -1,24 +1,20 @@
 package com.abooc.joker.dialog.samples;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.abooc.joker.dialog.R;
-import com.abooc.joker.dialog.ScanningDialog;
+import com.abooc.joker.dialog.ScannerSamples;
 import com.abooc.joker.dialog.UPnP;
-import com.abooc.joker.dialog.UPnPPresenter;
 import com.abooc.upnp.Discovery;
 import com.abooc.upnp.DlnaManager;
 import com.abooc.upnp.RendererPlayer;
-import com.abooc.upnp.model.DeviceDisplay;
 
 import org.fourthline.cling.support.model.Res;
 import org.fourthline.cling.support.model.item.Photo;
 
-public class SamplesActivity extends AppCompatActivity implements ScanningDialog.OnSelectedDeviceListener {
+public class SamplesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +25,8 @@ public class SamplesActivity extends AppCompatActivity implements ScanningDialog
         DlnaManager.getInstance().startService(SamplesActivity.this);
     }
 
-    @Override
-    public void onSelectedDevice(DeviceDisplay device) {
-
-        boolean bind = DlnaManager.getInstance().bind(device.getOriginDevice(), null);
-        if (bind) {
-            device.setChecked(true);
-
-            iScanningDialog.notifyDataSetChanged();
-        }
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                iScanningDialog.dismiss();
-            }
-        }, 300);
+    public void onShowScanningDialog(View view) {
+        ScannerSamples.show(this);
     }
 
     public void onFlyImage(View view) {
@@ -54,38 +36,11 @@ public class SamplesActivity extends AppCompatActivity implements ScanningDialog
     public void flyImage(String url) {
         url = "http://images.apple.com/v/home/cx/images/gallery/iphone_square_large.jpg";
         Res res = UPnP.buildRes("image/jpeg", "filePath", url, 0);
-        Photo videoItem = new Photo("1", String.valueOf(1), "图来了", "unknown", "unknown", res);
+        Photo videoItem = new Photo("1", String.valueOf(1), "The New iPhone7", "unknown", "unknown", res);
         String metadata = UPnP.buildMetadataXml(videoItem);
 
         RendererPlayer player = RendererPlayer.get();
         player.start(url, metadata);
-    }
-
-    UPnPPresenter iUPnPPresenter;
-    ScanningDialog iScanningDialog;
-
-    public void onShowScanningDialog(View view) {
-        iScanningDialog = new ScanningDialog(this);
-        iScanningDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                iScanningDialog.setOnSelectedDeviceListener(SamplesActivity.this);
-                iUPnPPresenter = new UPnPPresenter(iScanningDialog, new Handler());
-
-                iUPnPPresenter.start();
-            }
-        });
-
-        iScanningDialog.show();
-
-        iScanningDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-
-                iUPnPPresenter.quit();
-            }
-        });
-
     }
 
 }
