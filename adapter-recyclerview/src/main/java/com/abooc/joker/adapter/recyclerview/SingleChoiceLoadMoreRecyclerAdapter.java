2610f40e1682ab96c9c87a4ba6d855a645516350
zhangjunpu <junpu0311@163.com>
2017-03-15 14:33:54 +0800

    更新4recyclerView库版本v1.1.7

---
+++ adapter-recyclerview/src/main/java/com/abooc/joker/adapter/recyclerview/SingleChoiceLoadMoreRecyclerAdapter.java
@@ -0,0 +1,34 @@
+package com.abooc.joker.adapter.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * 单选列表
+ * @author zhangjunpu
+ *  15/8/10
+ */
+public abstract class SingleChoiceLoadMoreRecyclerAdapter<T> extends LoadMoreRecyclerAdapter<T> implements ViewHolder.OnRecyclerItemClickListener {
+
+    protected int mCheckedPosition = -1;
+
+    public SingleChoiceLoadMoreRecyclerAdapter(Context context) {
+        super(context);
+    }
+
+    public void setCheckedPosition(int position) {
+        this.mCheckedPosition = position;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(RecyclerView recyclerView, View itemView, int position) {
+        setCheckedPosition(position);
+
+        if (mListener != null) {
+            mListener.onItemClick(recyclerView, itemView, position);
+        }
+    }
+
+}
